06993fa0ddd6cd94aa6d52cb8539a10b8336402f
Harry Oyarvide <poyarvide87@yahoo.com>
2024-12-07 18:06:35 -0500

    schema. creating 3 tables

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+-- Departments table
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Roles table
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    salary NUMERIC NOT NULL,
+    department_id INT REFERENCES departments(id)
+);
+
+-- Employees table
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role_id INT REFERENCES roles(id),
+    manager_id INT REFERENCES employees(id)
+);
