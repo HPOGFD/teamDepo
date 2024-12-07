43e1c4ff62859347071918f39bd82a5cc197531d
Harry Oyarvide <poyarvide87@yahoo.com>
2024-12-07 17:58:05 -0500

    inquirer with main menu

---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import inquirer from 'inquirer';
+
+
+async function startApp() {
+  console.log('\nWelcome to the Employee Tracker!\n');
+
+  const mainMenu = async () => {
+    const {} = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit',
+        ],
+      },
+    ]);
+
+    // switch (action) {
+    //   case 'View all departments':
+    //     await viewDepartments();
+    //     break;
+    //   case 'View all roles':
+    //     await viewRoles();
+    //     break;
+    //   case 'View all employees':
+    //     await viewEmployees();
+    //     break;
+    //   case 'Add a department':
+    //     await addDepartment();
+    //     break;
+    //   case 'Add a role':
+    //     await addRole();
+    //     break;
+    //   case 'Add an employee':
+    //     await addEmployee();
+    //     break;
+    //   case 'Update an employee role':
+    //     await updateEmployeeRole();
+    //     break;
+    //   case 'Exit':
+    //     console.log('Goodbye!');
+    //     process.exit(0);
+    // }
+
+    mainMenu();
+  };
+
+  await mainMenu();
+}
+
+startApp();
